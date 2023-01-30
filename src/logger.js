e973173665763057ee8f27a4b2db23c406221f99
DenysMenfredy <denys.menfredy7@gmail.com>
2023-01-30 12:50:46 -0300

    basic config method to logger

--- src/logger.js
+++ src/logger.js
@@ -1,53 +1,67 @@
-
+const path = require('path');
 const fs = require('fs');
 
+
 class Logger {
-    constructor(saveToFile, startDate) {
-        this.saveToFile = saveToFile;
-        this.startDate = startDate;
-        if (saveToFile) {
-            this.logsFolder = './logs/'
-            if(!fs.existsSync(this.logsFolder)) {
-                fs.mkdirSync(this.logsFolder);
-            }
-            this.file = './logs/logs.log'
-            this.logStream = fs.createWriteStream(this.file, {flags: 'a'});
-        }
+    constructor() {
+        this.filename = null;
+        this.module = null;
+        this.logsFolder = './logs'
+        this.#createLogsDir();
+        this.loggingLevel = 'INFO'
+        this.file = 'logs.log';
+        this.logStream = fs.createWriteStream(`${this.logsFolder}/${this.file}`, {flags: 'a'});
     }
     info(text) {
         const date = new Date().toLocaleDateString('pt-Br');
         const hour = new Date().toLocaleTimeString('pt-Br');
         const log = `${date} - ${hour}: ${text}`;
-        if(this.saveToFile) {
-            this.#writeToFile(log);
-        }
+        this.#writeToFile(log);
         return console.info(log);
     }
     warning(text) {
         const date = new Date().toLocaleDateString('pt-Br');
         const hour = new Date().toLocaleTimeString('pt-Br');
         const log = `${date} - ${hour}: ${text}`;
-        if(this.saveToFile) {
-            this.#writeToFile(log);
-        }
+        this.#writeToFile(log);
         return console.warn(log);
     }
     error(text) {
         const date = new Date().toLocaleDateString('pt-Br');
         const hour = new Date().toLocaleTimeString('pt-Br');
         const log = `${date} - ${hour}: ${text}`;
-        if(this.saveToFile) {
-            this.#writeToFile(log);
-        }
+        this.#writeToFile(log);
         return console.info(log);
+    }
+    getLogger(moduleName) {
+        return new Logger()
+    }
+    basicConfig(filename, level) {
+        const splitedFilename = filename.split("/");
+        if(splitedFilename.length > 1) {
+            this.logsFolder = splitedFilename.at(0);
+            console.log(this.logsFolder);
+        } else {
+            this.file = splitedFilename.at(0);
+        }
+        this.#createLogsDir();
+        this.logStream = fs.createWriteStream(`${this.logsFolder}/${this.file}`, {flags: 'a'});
     }
     #writeToFile(data) {
         this.logStream.write(`${data}\n`);
     }
+    #createLogsDir() {
+        if(!fs.existsSync(this.logsFolder)) {
+            fs.mkdirSync(this.logsFolder);
+        }
+    }
 }
 
 
-const logger = new Logger(true);
+//TODO: implement logging level when saving to file
+
+const logger = new Logger();
+logger.basicConfig(filename='2newLog.log', 'INFO');
 logger.info("Oii");
 logger.warning("WARNING");
 setTimeout(() => {
